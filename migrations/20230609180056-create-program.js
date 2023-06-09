e29ef564a74b5ec4b5c0b4361617eebd73513cd3
DekiFirmansyah <dekifirmansyah99@gmail.com>
2023-06-10 01:32:39 +0700

    build web service fitur program

---
+++ migrations/20230609180056-create-program.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('programs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      organizer: {
+        type: Sequelize.STRING
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      date: {
+        type: Sequelize.STRING
+      },
+      kuota: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('programs');
+  }
+};
---
+++ models/program.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class program extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  program.init({
+    organizer: DataTypes.STRING,
+    title: DataTypes.STRING,
+    image: DataTypes.STRING,
+    date: DataTypes.STRING,
+    kuota: DataTypes.INTEGER,
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'program',
+  });
+  return program;
+};
