35a39bfc774fc8e41aa500ff2fa4ee1d1d2cbbd7
DekiFirmansyah <dekifirmansyah99@gmail.com>
2023-06-13 19:58:10 +0700

    update references table

--- migrations/20230612173334-create-volunteer.js
+++ migrations/20230612173334-create-volunteer.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'users',
+          model: 'user',
           key: 'id'
         }
       },
@@ -21,7 +21,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'programs',
+          model: 'program',
           key: 'id'
         }
       },
