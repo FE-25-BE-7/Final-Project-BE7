92812f62f7cda82c598220aea17d0b09cf2a9484
DekiFirmansyah <dekifirmansyah99@gmail.com>
2023-06-14 18:35:43 +0700

    update route artikel

--- routes/adminArtikelRouter.js
+++ routes/adminArtikelRouter.js
@@ -5,9 +5,9 @@ const {
     getAllArtikel,
     getDetailArtikel,
     createArtikel,
-    updateArtikel,
-    deleteAllArtikel,
-    deleteArtikel
+    // updateArtikel,
+    // deleteAllArtikel,
+    // deleteArtikel
 
 } = require("../controllers/adminArtikelController")
     // all user
@@ -16,8 +16,8 @@ const {
 router.get("/api/artikel", getAllArtikel);
 router.get("/api/artikel/:id", getDetailArtikel);
 router.post("/api/artikel", createArtikel);
-router.put("/api/artikel/:id", updateArtikel);
-router.delete("/api/artikel/delete", deleteAllArtikel);
-router.delete("/api/artikel/delete/:id", deleteArtikel);
+// router.put("/api/artikel/:id", updateArtikel);
+// router.delete("/api/artikel/delete", deleteAllArtikel);
+// router.delete("/api/artikel/delete/:id", deleteArtikel);
 
 module.exports = router;
