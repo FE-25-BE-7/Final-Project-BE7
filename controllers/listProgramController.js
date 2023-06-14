d8a9dff06e85abda01d39d32beffa76b9b334312
ilhamapik <ilhambrojol2@gmail.com>
2023-06-14 17:10:56 +0700

    list program fix

--- controllers/listProgramController.js
+++ controllers/listProgramController.js
@@ -1,43 +1,26 @@
-const program = require('../models').program
+const ListProgram = require('../models').ListProgram;
+
 module.exports = {
     getPrograms: async(req, res) => {
         try {
-            const programs = [{
-                    image_one: 'https://i.ibb.co/8P2Hh4y/donation-tree.jpg',
-                    image_two: 'https://i.ibb.co/hCBYQjC/d1.png',
-                    title: 'DONASI',
-                    sub_title: 'Bantu kami menjaga bumi kita',
-                    description: 'Mari membela lingkungan sepanjang hidupmu, bahkan lebih lama dari itu. Berdonasilah hari ini.',
-                    date: '8 - 10 Juli 2023',
-                    organizer: 'GreenPeace',
-                    link_name: 'Donasi Sekarang',
-                    id: '1',
-                },
-                {
-                    image_one: 'https://i.ibb.co/2tfKX7S/volunteer-teamwork.jpg',
-                    image_two: 'https://i.ibb.co/GdDvCx5/v1.png',
-                    title: 'RELAWAN',
-                    sub_title: 'Mari bergabung menjadi relawan',
-                    description: 'Bumi kita membutuhkan suara dan aksimu untuk mencari solusi membuat perubahan nyata.',
-                    date: '22 - 24 Juli 2023',
-                    organizer: 'GreenPeace',
-                    link_name: 'Daftar Sekarang',
-                    id: '2',
-                },
-            ];
-            res.json(programs);
+            // Mengambil semua data donasi yang berhasil
+            const programs = await ListProgram.findAll();
+
+            // Mengirimkan data donasi ke client
+            res.status(200).json(programs);
         } catch (error) {
-            console.error('Error getting programs:', error);
-            res.status(500).json({ error: 'Internal server error' });
+            // Menangani error jika terjadi
+            console.log('Error occurred:', error);
+            res.status(500).send({ error: error.message });
         }
     },
-
-
+    // add list
     addList: async(req, res) => {
         try {
-            const { image_one, image_two, title, sub_title, description, date, organizer, link_name, id } = req.body;
+            const { id, image_one, image_two, title, sub_title, description, date, organizer, link_name } = req.body;
 
             const newProgram = {
+                id,
                 image_one,
                 image_two,
                 title,
@@ -46,15 +29,17 @@ module.exports = {
                 date,
                 organizer,
                 link_name,
-                id,
             };
 
-            // Simpan atau lakukan operasi lain dengan data program yang diterima
+            // Simpan data program baru ke dalam database
+            const createdProgram = await ListProgram.create(newProgram);
 
-            res.json(newProgram);
+            res.status(201).json(createdProgram);
         } catch (error) {
             console.error('Error creating program:', error);
             res.status(500).json({ error: 'Internal server error' });
         }
     }
-}
+
+
+};
